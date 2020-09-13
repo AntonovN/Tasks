b8be42928735a5ad7ca25c298ebb979c8a0c8359
AntonovN <nikita.antonov0617@gmail.com>
2020-09-13 20:11:51 +0300

    Task_7

--- src/com/company/Task_7.java
+++ src/com/company/Task_7.java
@@ -2,7 +2,6 @@ package com.company;
 import java.util.Scanner;
 public class Task_7 {
     public static void main(String[] args) {
-        // write your code here
         int x, y, z;
         x = 3;
         y = 13;
@@ -18,7 +17,7 @@ public class Task_7 {
         } else if (number == z) {
             System.out.println("это чисто есть в константе");
         } else {
-            System.out.println("net takogo chisla v konstante");
+            System.out.println("нет такого числа в константе");
         }
     }
 }
