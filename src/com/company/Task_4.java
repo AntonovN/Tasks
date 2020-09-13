e9d1409be7eb3c690387f53a315a7a3ca6ffe9c5
AntonovN <nikita.antonov0617@gmail.com>
2020-09-13 20:16:52 +0300

    Task_4

---
+++ src/com/company/Task_4.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input binary number: ");
+        String bin = sc.nextLine();
+        int binNum = Integer.parseInt(bin, 2);
+
+        System.out.println(binNum);
+    }
+
+}
+
