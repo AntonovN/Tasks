09e2fd2cab163d7781ef55ec3055004cf3ea325a
AntonovN <nikita.antonov0617@gmail.com>
2020-09-14 13:25:15 +0300

    Task_5

--- src/com/company/Task_5.java
+++ src/com/company/Task_5.java
@@ -7,15 +7,15 @@ public class Task_5 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Ведите число: ");
         int x = sc.nextInt();
-        System.out.println(x + "*1" + "="+ x*1);
-        System.out.println(x + "*2" + "="+ x*2);
-        System.out.println(x + "*3" + "="+ x*3);
-        System.out.println(x + "*4" + "="+ x*4);
-        System.out.println(x + "*5" + "="+ x*5);
-        System.out.println(x + "*6" + "="+ x*6);
-        System.out.println(x + "*7" + "="+ x*7);
-        System.out.println(x + "*8" + "="+ x*8);
-        System.out.println(x + "*9" + "="+ x*9);
-        System.out.println(x + "*10" + "="+ x*10);
+        System.out.println(x + "*1" + "=" + x*1);
+        System.out.println(x + "*2" + "=" + x*2);
+        System.out.println(x + "*3" + "=" + x*3);
+        System.out.println(x + "*4" + "=" + x*4);
+        System.out.println(x + "*5" + "=" + x*5);
+        System.out.println(x + "*6" + "=" + x*6);
+        System.out.println(x + "*7" + "=" + x*7);
+        System.out.println(x + "*8" + "=" + x*8);
+        System.out.println(x + "*9" + "=" + x*9);
+        System.out.println(x + "*10" + "=" + x*10);
     }
 }
