3ba3f0bdd5990099ea15a68e8077bfd1ddf2f0d7
AntonovN <nikita.antonov0617@gmail.com>
2020-09-14 13:23:08 +0300

    Task_10

---
+++ src/com/company/Task_5.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ведите число:");
+        int x = sc.nextInt();
+        System.out.println(x + "*1" + "="+ x*1);
+        System.out.println(x + "*2" + "="+ x*2);
+        System.out.println(x + "*3" + "="+ x*3);
+        System.out.println(x + "*4" + "="+ x*4);
+        System.out.println(x + "*5" + "="+ x*5);
+        System.out.println(x + "*6" + "="+ x*6);
+        System.out.println(x + "*7" + "="+ x*7);
+        System.out.println(x + "*8" + "="+ x*8);
+        System.out.println(x + "*9" + "="+ x*9);
+        System.out.println(x + "*10" + "="+ x*10);
+    }
+}
