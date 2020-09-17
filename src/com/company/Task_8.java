811ec07631490bd6856f21232c9bba564e94801b
AntonovN <nikita.antonov0617@gmail.com>
2020-09-17 20:17:16 +0300

    Task_8 changes

--- src/com/company/Task_8.java
+++ src/com/company/Task_8.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Task_8 {
     public static void main(String[] args) {
-        for(int i=1; i<=50; i++){
+        for (int i = 1; i <= 50; i++) {
             System.out.println(i);
         }
     }
