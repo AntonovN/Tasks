574ff9919786e935afa9738ea9866739fdbc8cbb
AntonovN <nikita.antonov0617@gmail.com>
2020-09-17 19:58:40 +0300

    Task_6 changes

--- src/com/company/Task_6.java
+++ src/com/company/Task_6.java
@@ -1,8 +1,10 @@
 package com.company;
+
 import java.util.Scanner;
+
 public class Task_6 {
     public static void main(String[] args) {
-        // write your code here
+
         Scanner in = new Scanner(System.in);
         System.out.print("Input x number: ");
         int x = in.nextInt();
@@ -11,16 +13,15 @@ public class Task_6 {
         System.out.print("Input z number: ");
         int z = in.nextInt();
 
-        double res = (x*y*z)/3;
-
+        double res = (x * y * z) / 3;
 
 
         System.out.println(res);
 
-        int res2 = (int)res/2;
+        int res2 = (int) res / 2;
         System.out.println(res2);
 
-        if (res2>3) {
+        if (res2 > 3) {
             System.out.println("Программа выполнена корректно");
         }
 
